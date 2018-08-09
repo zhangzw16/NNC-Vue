fd6f4bccf280fd3172c7c74597f41632a19669c0
zhangzw16 <thuzhangzw@outlook.com>
2018-08-09 10:37:07 +0800

    add three btns to filter user

--- vue-project/src/components/page_4/index.js
+++ vue-project/src/components/page_4/index.js
@@ -19,13 +19,39 @@ export default {
     })
     .then((res) => {
       this.tableData4 = res.data.data.list;
+      console.log(this.tableData4);
 
       for(let i = 0; i < this.tableData4.length; i++) {
-        console.log(this.tableData4[i].id);
+        // console.log(this.tableData4[i].id);
       }
     })
     .catch(err => {
       // console.log(err);
     })
+  },
+  methods: {
+    userFilter(status) {
+      console.log(status);
+      this.axios({
+        method: 'post',
+        url: '/NNC/rest/user_Info/user_info_page_on_dietitian',
+        params: {
+          page: '1',
+          userStatus: status,
+          dietitianId: this.$router.history.current.query.dietitianId
+        }
+      })
+      .then((res) => {
+        this.tableData4 = res.data.data.list;
+        console.log(this.tableData4);
+  
+        for(let i = 0; i < this.tableData4.length; i++) {
+          // console.log(this.tableData4[i].id);
+        }
+      })
+      .catch(err => {
+        // console.log(err);
+      })
+    }
   }
 }
--- vue-project/src/components/page_4/index.vue
+++ vue-project/src/components/page_4/index.vue
@@ -1,39 +1,60 @@
 <template>
-   <el-table 
-        :data="tableData4"
-        style="width: 100%"
-        :row-class-name="tableRowClassName">
-        <el-table-column
-            prop="id"
-            label="序号"
-            width="80">
-        </el-table-column>
-        <el-table-column
-            prop="name"
-            label="客户姓名"
-            width="140">
-        </el-table-column>
-        <el-table-column
-            prop="phoneNo"
-            label="手机号"
-            width="140">
-        </el-table-column>
-            <el-table-column
-            prop="passwd"
-            label="减重日"
-            width="200">
-        </el-table-column>
-        <el-table-column
-            prop=""
-            label="详细数据"
-            width="140">
-            <template slot-scope="scope">
-            <el-button
-                size="mini"
-                @click="handleEdit(scope.$index, scope.row)">查看</el-button>
-            </template>
-        </el-table-column>
-        </el-table>
+    <el-row id="page_1">
+        <el-col :span="24">
+            <el-row>
+                <el-button type="primary"
+                    @click="userFilter(1)">
+                    正在减脂客户
+                </el-button>
+                <el-button type="primary"
+                    @click.native="userFilter(2)">
+                    过渡期客户
+                </el-button>
+                <el-button type="primary"
+                    @click.native="userFilter(3)">
+                    已完成客户
+                </el-button>
+                <newSystemUserAdd></newSystemUserAdd>
+            </el-row>
+            <el-table 
+                :data="tableData4"
+                style="width: 100%"
+                :row-class-name="tableRowClassName">
+                <el-table-column
+                    prop="id"
+                    label="序号"
+                    width="80">
+                </el-table-column>
+                <el-table-column
+                    prop="name"
+                    label="客户姓名"
+                    width="140">
+                </el-table-column>
+                <el-table-column
+                    prop="phoneNo"
+                    label="手机号"
+                    width="140">
+                </el-table-column>
+                    <el-table-column
+                    prop="passwd"
+                    label="减重日"
+                    width="200">
+                </el-table-column>
+                <el-table-column
+                    prop=""
+                    label="详细数据"
+                    width="140">
+                    <template slot-scope="scope">
+                    <el-button
+                        size="mini"
+                        @click="handleEdit(scope.$index, scope.row)">查看</el-button>
+                    </template>
+                </el-table-column>
+            </el-table>
+        </el-col>
+    </el-row>
 </template>
 <style src="./index.less" lang="less"></style>
 <script src="./index.js"></script>
+
+
