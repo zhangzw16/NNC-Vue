234141e746742c85eef9c264d06f6a003505ef2b
llp16 <39506793+llp16@users.noreply.github.com>
2018-08-09 16:23:07 +0800

    header css

--- vue-project/src/components/header/index.vue
+++ vue-project/src/components/header/index.vue
@@ -1,6 +1,15 @@
 <template>
-  <div>
-    尼基营养
+  <div id="header">
+    <div class="left_header">
+      <img alt src="../../../res/ncclogo.png" 
+        width="36" 
+        height="36" 
+        class="ncc_logo">
+      <span class="default_company_name_boco">尼基营养</span>
+      <span class="default_company_name_title">管理平台</span>
+    </div>
+    <div class="right_header">
+    </div>
   </div>
 </template>
 <style src="./index.less" lang="less"></style>
