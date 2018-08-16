b3cdbc54e3faf8ef19fa2496ec93e2a2ca265af0
llp16 <39506793+llp16@users.noreply.github.com>
2018-08-16 17:10:51 +0800

    page5 AgePiePhase

--- vue-project/src/components/page_5/index.js
+++ vue-project/src/components/page_5/index.js
@@ -6,27 +6,127 @@ export default {
   
   data () {
     return {
-
+        pageData: null,
+        numOfMen: [],
+        numOfWomen: [],
+        numOfAll: [],
+        radio: "2"
     }
   },
   created() {
     this.refresh();
+
+    let self = this;
+    setTimeout(
+        function(){
+            self.drawSexPiePhase(self.numOfAll, "全部年龄分布")
+        },500);
   },
   methods: {
     refresh() {
-    //   console.log("refresh");
-    //   this.axios({
-    //     method: 'post',
-    //     url: '',
-    //     data: {
+      this.getSexData();
+    },
 
-    //     }
-    //   })
-    //   .then((res) => {
+    sexChange(value){
+        let self = this;
+        if(value == 0){
+            setTimeout(
+                function(){
+                    self.drawSexPiePhase(self.numOfMen, "男性年龄分布")
+                },500);           
+        }
+        else if(value == 1){
+            setTimeout(
+                function(){
+                    self.drawSexPiePhase(self.numOfWomen, "女性年龄分布")
+                },500);
+        }
+        else{
+            setTimeout(
+                function(){
+                    self.drawSexPiePhase(self.numOfAll, "全部年龄分布")
+                },500);
+        }
+    },
 
-    //   })
-    //   .catch(err => {
-    //   });
+    getSexData(){
+        this.axios({
+            method: 'post',
+            url: "/NNC/rest/user_Info/get_user_report_forms",
+            })
+            .then((res) => {
+            this.pageData = res.data.data;
+            let nameList = ['0-17','18-29','30-39','40-49','50-59','60'];
+            
+            let menData = null,
+                womenData = null;
+
+            menData = res.data.data.numOfMen;
+            womenData = res.data.data.numOfWomen;
+                
+            for(let i of nameList){
+                // this.numOfMen[index].value = menData[i],
+                // this.numOfMen[index].name = i;
+                console.log(i);
+                this.numOfMen.push({"value" : menData[i], "name" : i});
+                this.numOfWomen.push({"value" : womenData[i], "name" : i});
+                this.numOfAll.push({"value" : womenData[i] + menData[i], "name" : i})
+            }
+
+          })
+          .catch(err => {
+            console.log(err);
+          })
+    },
+
+    drawSexPiePhase(data, sex) {
+      let myChart = this.$echarts.init(document.getElementById('piePhase'))
+      myChart.setOption({
+        title : {
+          text: sex,
+          x:'center'
+      },
+      tooltip : {
+          trigger: 'item',
+          formatter: "{a} <br/>{b} : {c} ({d}%)"
+      },
+      legend: {
+          orient : 'vertical',
+          x : 'left',
+          data:['0-17','18-29','30-39','40-49','50-59','60']
+      },
+      toolbox: {
+          show : true,
+          feature : {
+              mark : {show: true},
+              dataView : {show: true, readOnly: false},
+              magicType : {
+                  show: true, 
+                  type: ['pie', 'funnel'],
+                  option: {
+                      funnel: {
+                          x: '25%',
+                          width: '50%',
+                          funnelAlign: 'left',
+                          max: 1548
+                      }
+                  }
+              },
+              restore : {show: true},
+              saveAsImage : {show: true}
+          }
+      },
+      calculable : true,
+      series : [
+          {
+              name:'年龄段',
+              type:'pie',
+              radius : '55%',
+              center: ['50%', '60%'],
+              data: data         
+          }
+      ]
+      })
     }
   }
 }
--- vue-project/src/components/page_5/index.vue
+++ vue-project/src/components/page_5/index.vue
@@ -1,8 +1,19 @@
 <template>
-  <el-row id="page_5" style="height: 100%">
+   <el-row id="page_5" style="height: 100%">			
 		<el-row>
-			<el-button>点我</el-button>
-		</el-row>	
+			<el-card>
+				<el-form ref="form" :model="form" label-width="200px">
+					<el-form-item label="性别占比：">
+						<el-radio-group v-model="radio" @change="sexChange">
+							<el-radio-button label="2">全部</el-radio-button>
+							<el-radio-button label="0">男性</el-radio-button>
+							<el-radio-button label="1">女性</el-radio-button>
+						</el-radio-group>
+					</el-form-item>
+				</el-form>
+				<div id="piePhase" style="width: 620px; height: 280px;"></div>
+			</el-card>
+		</el-row>
 	</el-row>
 </template>
 <style src="./index.less" lang="less"></style>
